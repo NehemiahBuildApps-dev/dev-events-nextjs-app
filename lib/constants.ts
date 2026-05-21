d6ac5ecc627a049e4ed2419e574d07f3df0cd7ff
Nehemiah <nehemiahadewumi95@gmail.com>
2026-05-22 00:32:06 +0100

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+    {
+        title: "Next.js Conf 2026",
+        image: "/images/event1.png",
+        slug: "nextjs-conf-2026",
+        location: "San Francisco, CA",
+        date: "2026-10-25",
+        time: "10:00 AM"
+    },
+    {
+        title: "React Summit",
+        image: "/images/event2.png",
+        slug: "react-summit",
+        location: "Amsterdam, NL",
+        date: "2026-06-12",
+        time: "09:00 AM"
+    },
+    {
+        title: "Web3 Hackathon",
+        image: "/images/event3.png",
+        slug: "web3-hackathon",
+        location: "Remote",
+        date: "2026-07-15",
+        time: "11:00 AM"
+    },
+    {
+        title: "JS World Conference",
+        image: "/images/event4.png",
+        slug: "js-world-conference",
+        location: "Berlin, DE",
+        date: "2026-09-05",
+        time: "08:30 AM"
+    },
+    {
+        title: "AI Dev Expo",
+        image: "/images/event5.png",
+        slug: "ai-dev-expo",
+        location: "London, UK",
+        date: "2026-11-10",
+        time: "09:30 AM"
+    },
+    {
+        title: "DevOps Days",
+        image: "/images/event6.png",
+        slug: "devops-days",
+        location: "Austin, TX",
+        date: "2026-08-20",
+        time: "10:00 AM"
+    }
+];
